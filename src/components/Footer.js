3f632d675f8248c2bc069b22a0ab2ccdfbe2767b
Jus' A Mere Dev <sandilemoremedi070501@gmail.com>
2022-03-03 10:53:54 +0200

    The basic files

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <h1>Mr. Young Joey</h1>
+      <div className="media">
+        <div className="instagram"></div>
+        <div className="soundCloud"></div>
+      </div>
+    </footer>
+  );
+}
---
+++ src/components/Home/Home.js
@@ -0,0 +1,7 @@
+export default function Home(){
+    return(
+        <div className="home">
+            <h1>Mr. Young Joey</h1>
+        </div>
+    )
+}
