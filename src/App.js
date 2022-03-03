27baa60d8ce79691368acfd7e82f9abf372edd96
Jus' A Mere Dev <sandilemoremedi070501@gmail.com>
2022-03-03 11:24:55 +0200

    Added the basic navigations

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Discography from "./components/Discography/Discography";
+import Contact from "./components/Contact/Contact";
+import News from "./components/News/News";
 
 function App() {
   return (
-    <Routes>
-      <Route element={<Home />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/discography" element={<Discography />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/news" element={<News />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
